3c4d381cbb8cce5edd81f4e2db6bac5bdbac7ccb
EragonsCoding <jontyvanblerk23@gmail.com>
2025-10-13 17:05:37 +0200

    Andre Changed

--- app/src/main/java/com/example/wattway_app/LoadsheddingActivity.java
+++ app/src/main/java/com/example/wattway_app/LoadsheddingActivity.java
@@ -1,15 +1,11 @@
 package com.example.wattway_app;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LoadsheddingActivity extends AppCompatActivity {
@@ -17,14 +13,13 @@ public class LoadsheddingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loadshedding);
+
         setupBottomNav();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // You can set data later like:
+        // ((TextView) findViewById(R.id.tvSelectedArea)).setText("Your area");
+        // ((Chip) findViewById(R.id.chipStage)).setText("Stage 3");
     }
 
     private void setupBottomNav() {
@@ -35,21 +30,15 @@ public class LoadsheddingActivity extends AppCompatActivity {
             int id = item.getItemId();
             if (id == R.id.nav_map) {
                 startActivity(new Intent(this, HomePageActivity.class));
-                overridePendingTransition(0, 0);
-                finish();
-                return true;
+                overridePendingTransition(0, 0); finish(); return true;
             } else if (id == R.id.nav_stations) {
                 startActivity(new Intent(this, StationsActivity.class));
-                overridePendingTransition(0, 0);
-                finish();
-                return true;
+                overridePendingTransition(0, 0); finish(); return true;
             } else if (id == R.id.nav_load) {
                 return true;
             } else if (id == R.id.nav_profile) {
                 startActivity(new Intent(this, ProfileActivity.class));
-                overridePendingTransition(0, 0);
-                finish();
-                return true;
+                overridePendingTransition(0, 0); finish(); return true;
             }
             return false;
         });
